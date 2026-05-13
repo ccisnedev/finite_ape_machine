d5358a48e43d9c6b8ed1f48c8d8a8fd67f34e6ed
ccisnedev <ccisnedev@gmail.com>
2026-05-13 01:32:05 -0500

    fix(#186): add Invoke-ExpertCouncil to assets_test skill list

--- code/cli/test/assets_test.dart
+++ code/cli/test/assets_test.dart
@@ -151,6 +151,7 @@ void main() {
       expect(
         dirs,
         unorderedEquals([
+          'Invoke-ExpertCouncil',
           'doc-read',
           'doc-write',
           'inquiry-install',
